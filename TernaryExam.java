280cf87300a0a3029ed35f54e15598cbefce3635
whitenugul <andrewahn00@gmail.com>
2021-08-06 21:20:10 +0900

    Create TernaryExam.java

---
+++ TernaryExam.java
@@ -0,0 +1,11 @@
+public class TernaryExam {
+    public static void main(String[] args) {
+        // int b1 = (5 > 4) ? 50: 40;
+        // System.out.println(b1);
+        if(5 > 4){
+            System.out.println(50);
+        }else{
+            System.out.println(40);
+        }
+    }
+}
